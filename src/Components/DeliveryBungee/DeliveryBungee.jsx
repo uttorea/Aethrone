a6edf2089477ef786a608f1f805c5138acae183c
Kunal <deshwa170@gmail.com>
2024-07-08 23:51:00 +0530

    Fixed Launcher Systems Page

--- src/Components/DeliveryBungee/DeliveryBungee.jsx
+++ src/Components/DeliveryBungee/DeliveryBungee.jsx
@@ -1,17 +1,58 @@
-import React from 'react'
-import './DeliveryBungee.css'
-import bungeeImg from '../../assets/bungeeImg.png'
+import React, { useEffect } from 'react';
+import './DeliveryBungee.css';
+import bungeeImg from '../../assets/bungeeImg.png';
+import bungeeImg1 from '../../assets/BungeeImg1.png';
+import bungeeImg2 from '../../assets/BungeeImg2.png';
+
 const DeliveryBungee = () => {
+    useEffect(() => {
+        // Intersection Observer API to handle image animations
+        const observerOptions = {
+            threshold: 0.5 // Adjust based on when you want the animation to trigger
+        };
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('visible');
+                } else {
+                    entry.target.classList.remove('visible');
+                }
+            });
+        }, observerOptions);
+
+        const images = document.querySelectorAll('.scroll_img');
+        const headings = document.querySelectorAll('.scroll_heading');
+
+        images.forEach(image => {
+            observer.observe(image);
+        });
+
+        headings.forEach(heading => {
+            observer.observe(heading);
+        });
+
+        return () => {
+            // Clean up the observer
+            images.forEach(image => {
+                observer.unobserve(image);
+            });
+            headings.forEach(heading => {
+                observer.unobserve(heading);
+            });
+        };
+    }, []);
+
     return (
         <div className='container' style={{marginTop:'100px'}}>
-            <h4 className='border-start border-primary border-5 px-2 fw-bold m-0'>Bungee Catapult Launcher ~ YPJ 30</h4>
-            <small className='px-3'>Specification</small>
-            <div class="row">
-                <div class="col-sm-6">
-                    <div class="card border-0">
-                        <div class="card-body ">
-                            <p class="card-text">This Custom Off The Shelf Launcher can be used to launch upto 30 kgs of Fixed Wing UAVs.</p>
-                            <table className=' w-100'>
+            <h4 className='border-start border-primary border-5 px-2 fw-bold m-0 scroll_heading'>Bungee Catapult Launcher ~ YPJ 30</h4>
+            <small className='px-3 scroll_heading'>Specification</small>
+            <div className="row">
+                <div className="col-sm-6">
+                    <div className="card border-0">
+                        <div className="card-body">
+                            <p className="card-text scroll_heading">This Custom Off The Shelf Launcher can be used to launch upto 30 kgs of Fixed Wing UAVs.</p>
+                            <table className=' w-100 scroll_heading'>
                                 <tr className=''>
                                     <td className='border-1 text-start px-5 p-3'>Maximum Aircraft Weight <br />Corresponding Max Velocity</td>
                                     <td className='border-1 text-start px-5'>30 kgs / 30m/s</td>
@@ -34,16 +75,38 @@ const DeliveryBungee = () => {
                                 </tr>
                             </table>
                         </div>
+                        <div className="card-body">
+                        </div>
+                        <div className="card-body">
+                          <h1 id='seID' className="card-text scroll_heading">100</h1>
+                            <p className="card-text scroll_heading">Launches per bungee</p>
+                        </div>
+                        <div className="card-body" style={{marginTop:'250px'}}>
+                          <h1 id='seID' className="card-text scroll_heading">Features</h1>
+                          <ul className="card-text scroll_heading">
+                            <li>Configurable Trolley to accomodate</li>
+                            <li>launch Speed Indicated</li>
+                            <li>Variable Angle of Launch</li>
+                            <li>Automated Launching Mechanism</li>
+                            <li>Built Capability for upto 100 launches</li>
+
+                            </ul>                        </div>
                     </div>
                 </div>
-                <div class="col-sm-6">
-                    <div class="card border-0 px-5 mt-4">
-                            <img src={bungeeImg} alt="" className=' shadow p-3 mb-5 bg-body rounded border-start border-top border-primary' />
+                <div className="col-sm-6">
+                    <div className="card border-0 px-5 mt-4">
+                        <img src={bungeeImg} alt="" className='shadow p-3 mb-5 bg-body rounded border-start border-top border-primary scroll_img' />
+                    </div>
+                    <div className="card border-0 px-5 mt-4">
+                        <img style={{width:'500px'}} src={bungeeImg1} alt="" className='shadow p-3 mb-5 bg-body rounded border-start border-top border-primary scroll_img' />
+                    </div>
+                    <div className="card border-0 px-5 mt-4">
+                        <img style={{width:'500px'}} src={bungeeImg2} alt="" className='shadow p-3 mb-5 bg-body rounded border-start border-top border-primary scroll_img' />
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default DeliveryBungee
+export default DeliveryBungee;
--- src/Components/TubeLauncher/TubeLauncher.jsx
+++ src/Components/TubeLauncher/TubeLauncher.jsx
@@ -1,139 +1,112 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import tubeLauncher from '../../assets/tubeLauncher.png'
+import tubeLauncher1 from '../../assets/Tubelauncher1.png'
+import tubeLauncher2 from '../../assets/Tubelauncher2.png'
 import './TubeLauncher.css'
 const TubeLauncher = () => {
-    return (
-        <>
-            <div className='container' style={{ marginTop: '100px' }}>
-                <h4 className='border-start border-primary border-5 px-2 fw-bold m-0'>Tube Launcher ~ FBM10</h4>
-                <small className='px-3'>Specification</small>
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div class="card border-0">
-                            <div class="card-body ">
-                                <p class="card-text">This Custom Off The Shelf Launcher can be used to launch upto 10 kgs of Foldable Wing UAVs</p>
-                                <table className=' w-100'>
-                                    <tr className=''>
-                                        <td className='border-1 text-start px-5 p-3'>Maximum Aircraft Weight</td>
-                                        <td className='border-1 text-start px-5'>10 kgs</td>
-                                    </tr>
-                                    <tr>
-                                        <td className='border-1 text-start px-5 p-3'>Corresponding Max Velocity</td>
-                                        <td className='border-1 text-start px-5'>35 m/s</td>
-                                    </tr>
-                                    <tr>
-                                        <td className='border-1 text-start px-5 p-3'>G load on aircraft</td>
-                                        <td className='border-1 text-start px-5'>20G</td>
-                                    </tr>
-                                    <tr>
-                                        <td className='border-1 text-start px-5 p-3'>Launch Angle</td>
-                                        <td className='border-1 text-start px-5'>15 - 35 Deg</td>
-                                    </tr>
-                                    <tr>
-                                        <td className='border-1 text-start px-5 p-3'>Operating Temperature</td>
-                                        <td className='border-1 text-start px-5'>-10 to +50 Deg</td>
-                                    </tr>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="card border-0 px-5 mt-4">
-                            <img src={tubeLauncher} alt="" className=' rounded-3 ' />
-                        </div>
-                    </div>
-                </div>
-                <div className='mt-5'>
-                    <h4 className='border-start border-primary border-5 px-2 fw-bold m-0'>Tube Launcher ~ FBM10</h4>
-                    <small className='px-3'>Specification</small>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="card border-0">
-                                <div class="card-body ">
-                                    <p class="card-text">This Custom Off The Shelf Launcher can be used to launch upto 10 kgs of Foldable Wing UAVs</p>
-                                    <table className=' w-100'>
-                                        <tr className=''>
-                                            <td className='border-1 text-start px-5 p-3'>Maximum Aircraft Weight</td>
-                                            <td className='border-1 text-start px-5'>10 kgs</td>
-                                        </tr>
-                                        <tr>
-                                            <td className='border-1 text-start px-5 p-3'>Corresponding Max Velocity</td>
-                                            <td className='border-1 text-start px-5'>35 m/s</td>
-                                        </tr>
-                                        <tr>
-                                            <td className='border-1 text-start px-5 p-3'>G load on aircraft</td>
-                                            <td className='border-1 text-start px-5'>20G</td>
-                                        </tr>
-                                        <tr>
-                                            <td className='border-1 text-start px-5 p-3'>Launch Angle</td>
-                                            <td className='border-1 text-start px-5'>15 - 35 Deg</td>
-                                        </tr>
-                                        <tr>
-                                            <td className='border-1 text-start px-5 p-3'>Operating Temperature</td>
-                                            <td className='border-1 text-start px-5'>-10 to +50 Deg</td>
-                                        </tr>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
+  useEffect(() => {
+    // Intersection Observer API to handle image animations
+    const observerOptions = {
+        threshold: 0.5 // Adjust based on when you want the animation to trigger
+    };
 
-                        <div class="col-sm-6">
-                            <div class="card border-0 px-5 mt-4">
-                                <img src={tubeLauncher} alt="" className=' rounded-3 ' />
-                            </div>
-                        </div>
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, observerOptions);
+
+    const images = document.querySelectorAll('.scroll_img');
+    const headings = document.querySelectorAll('.scroll_heading');
+
+    images.forEach(image => {
+        observer.observe(image);
+    });
+
+    headings.forEach(heading => {
+        observer.observe(heading);
+    });
+
+    return () => {
+        // Clean up the observer
+        images.forEach(image => {
+            observer.unobserve(image);
+        });
+        headings.forEach(heading => {
+            observer.unobserve(heading);
+        });
+    };
+}, []);
+
+return (
+    <div className='container' style={{marginTop:'100px'}}>
+        <h4 className='border-start border-primary border-5 px-2 fw-bold m-0 scroll_heading'>Tube Launcher ~ FBM10</h4>
+        <small className='px-3 scroll_heading'>Specification</small>
+        <div className="row">
+            <div className="col-sm-6">
+                <div className="card border-0">
+                    <div className="card-body">
+                        <p className="card-text scroll_heading">This Custom Off The Shelf Launcher can be used to launch upto 10 kgs of Foldable Wing UAVs.</p>
+                        <table className=' w-100 scroll_heading'>
+                            <tr className=''>
+                                <td className='border-1 text-start px-5 p-3'>Maximum Aircraft Weight <br />Corresponding Max Velocity</td>
+                                <td className='border-1 text-start px-5'>10 kgs</td>
+                            </tr>
+                            <tr>
+                                <td className='border-1 text-start px-5 p-3'>G load on aircraft</td>
+                                <td className='border-1 text-start px-5'>35 m/s</td>
+                            </tr>
+                            <tr>
+                                <td className='border-1 text-start px-5 p-3'>Launcher Weight</td>
+                                <td className='border-1 text-start px-5'>20 G</td>
+                            </tr>
+                            <tr>
+                                <td className='border-1 text-start px-5 p-3'>Launch Angle</td>
+                                <td className='border-1 text-start px-5'>15 - 35 Deg</td>
+                            </tr>
+                            <tr>
+                                <td className='border-1 text-start px-5 p-3'>Indigenous Content</td>
+                                <td className='border-1 text-start px-5'>-10 to +50 Deg</td>
+                            </tr>
+                        </table>
                     </div>
-                </div>
-                <div className='mt-5'>
-                    <h4 className='border-start border-primary border-5 px-2 fw-bold m-0'>Tube Launcher ~ FBM10</h4>
-                    <small className='px-3'>Specification</small>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="card border-0">
-                                <div class="card-body ">
-                                    <p class="card-text">This Custom Off The Shelf Launcher can be used to launch upto 10 kgs of Foldable Wing UAVs</p>
-                                    <table className=' w-100'>
-                                        <tr className=''>
-                                            <td className='border-1 text-start px-5 p-3'>Maximum Aircraft Weight</td>
-                                            <td className='border-1 text-start px-5'>10 kgs</td>
-                                        </tr>
-                                        <tr>
-                                            <td className='border-1 text-start px-5 p-3'>Corresponding Max Velocity</td>
-                                            <td className='border-1 text-start px-5'>35 m/s</td>
-                                        </tr>
-                                        <tr>
-                                            <td className='border-1 text-start px-5 p-3'>G load on aircraft</td>
-                                            <td className='border-1 text-start px-5'>20G</td>
-                                        </tr>
-                                        <tr>
-                                            <td className='border-1 text-start px-5 p-3'>Launch Angle</td>
-                                            <td className='border-1 text-start px-5'>15 - 35 Deg</td>
-                                        </tr>
-                                        <tr>
-                                            <td className='border-1 text-start px-5 p-3'>Operating Temperature</td>
-                                            <td className='border-1 text-start px-5'>-10 to +50 Deg</td>
-                                        </tr>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="card border-0 px-5 mt-4">
-                                <img src={tubeLauncher} alt="" className=' rounded-3 ' />
-                            </div>
-                        </div>
+                    <div className="card-body">
                     </div>
+                    <div className="card-body">
+                      <h1 id='seID' className="card-text scroll_heading">1000</h1>
+                        <p className="card-text scroll_heading">Number of Launches </p>
+                    </div>
+                    <div className="card-body" style={{marginTop:'250px'}}>
+                      <h1 id='seID' className="card-text scroll_heading">Features</h1>
+                      <ul className="card-text scroll_heading">
+                        <li>Configurable Tube for Various Diameter of UVAs upto 210 mm ID</li>
+                        <li>Man Portable with cammmo bag</li>
+                        <li>Recoil reduction elastomers</li>
+                        <li>Sensor to shooter Integration Capable</li>
+                        <li>ATWAD TM system for foldable propellers of UAV</li>
+
+                        </ul>                        </div>
                 </div>
             </div>
-            <div className="launcherAction border-top border-1 border-black">
-                <h3 className='border-start border-primary border-5 px-2 fw-bold m-0'>See The Launcher In Action</h3>
-                <div className="bungeeAction">
-                    <a href="" className='btn btn-primary px-5 py-3 mt-2'> Contact Us </a>
-                    <a href="" className='btn border-primary mx-3 px-4 py-3 text-primary mt-2'> Download Brocher </a>
+            <div className="col-sm-6">
+                <div className="card border-0 px-5 mt-4">
+                    <img src={tubeLauncher} alt="" className='shadow p-3 mb-5 bg-body rounded border-start border-top border-primary scroll_img' />
+                </div>
+                <div className="card border-0 px-5 mt-4">
+                    <img style={{width:'500px'}} src={tubeLauncher1} alt="" className='shadow p-3 mb-5 bg-body rounded border-start border-top border-primary scroll_img' />
+                </div>
+                <div className="card border-0 px-5 mt-4">
+                    <img style={{width:'500px'}} src={tubeLauncher2} alt="" className='shadow p-3 mb-5 bg-body rounded border-start border-top border-primary scroll_img' />
                 </div>
             </div>
-        </>
-    )
+        </div>
+    </div>
+);
 }
+
 
 export default TubeLauncher
