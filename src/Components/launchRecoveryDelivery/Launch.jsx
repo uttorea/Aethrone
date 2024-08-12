1ba317471744f51c3bd3940413c4d482bf02e370
Amrendra chaudhary <91556894+amrendra16@users.noreply.github.com>
2024-08-13 00:12:15 +0530

    button correction

--- src/Components/launchRecoveryDelivery/Launch.jsx
+++ src/Components/launchRecoveryDelivery/Launch.jsx
@@ -36,13 +36,13 @@ const Launch = () => {
               </div>
             </div>
             <div className="launch_btn mt-3 mt-md-5">
-              <a
+              <Link
                 className="btn btn-light  px-md-5 px-1 ms-0 ms-md-4 launch_btn1 fontfamilySecondary maincolor"
-                href="/delivery-launcher-system"
+                to="/delivery-launcher-system"
               >
                 {" "}
                 Launcher System <i className="bi bi-arrow-down"></i>
-              </a>
+              </Link>
               <Link
                 className="btn btn-light px-md-5 px-1 mx-md-5 mx-1 launch_btn1 maincolor fontfamilySecondary"
                 to="/launch-recovery-system"
