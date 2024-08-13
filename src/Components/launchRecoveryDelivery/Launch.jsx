ee9304e24a02da9f243e3b19e3c6d868c5542ae5
Amrendra chaudhary <91556894+amrendra16@users.noreply.github.com>
2024-08-13 11:17:43 +0530

    f

--- src/Components/launchRecoveryDelivery/Launch.jsx
+++ src/Components/launchRecoveryDelivery/Launch.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { Link } from "react-router-dom";
+// import React from "react";
+// import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Launch.css";
 import HeadingComponent from "../HeadingComponent/HeadingComponent";
 import launchbgimg from "../../assets/launchbgimg.png";
@@ -7,14 +9,14 @@ import LauncherSystem from "../LauncherSystem/LauncherSystem";
 import LauncherRecovery from "../LauncherRecoverySystem/LauncherRecovery";
 import Project from "../Project/Project";
 import Footer from "../../Components/Footer/Footer";
-
 const Launch = () => {
+  const navigate = useNavigate();
+
   const handleLauncherClick = (e) => {
     e.preventDefault(); // Prevent the default behavior
-    window.location.replace("/delivery-launcher-system");
+    navigate("/delivery-launcher-system", { replace: true });
+    window.location.reload(); // Reload the page after navigating
   };
-  
-
   return (
     <>
       <div className="launch_container">
@@ -27,7 +29,7 @@ const Launch = () => {
             <div className="mt-5">
               <HeadingComponent heading="What We Do" />
               <h3 className="lrdimg_para1 mt-md-3 mt-2 px-2"></h3>
-              <div className="lrdimg_para2 p-md-5 p-2 rounded mt-3 me-2 me-md-0">
+              <div className="lrdimg_para2 p-md-5 p-2   rounded mt-3 me-2 me-md-0">
                 <p className="fontsecondry letterSpacing px-md-4 px-2">
                   We design and manufacture end-to-end solutions for launch and
                   recovery/delivery of UAVs and Lighter Than Air
@@ -43,17 +45,18 @@ const Launch = () => {
               </div>
             </div>
             <div className="launch_btn mt-3 mt-md-5">
-              <Link
-                className="btn btn-light px-md-5 px-1 ms-0 ms-md-4 launch_btn1 fontfamilySecondary maincolor"
-                to="/delivery-launcher-system"
-                onClick={handleLauncherClick}
-              >
-                Launcher System <i className="bi bi-arrow-down"></i>
-              </Link>
+            <Link
+      className="btn btn-light px-md-5 px-1 ms-0 ms-md-4 launch_btn1 fontfamilySecondary maincolor"
+      to="/delivery-launcher-system"
+      onClick={handleLauncherClick}
+    >
+      Launcher System <i className="bi bi-arrow-down"></i>
+    </Link>
               <Link
                 className="btn btn-light px-md-5 px-1 mx-md-5 mx-1 launch_btn1 maincolor fontfamilySecondary"
                 to="/launch-recovery-system"
               >
+                {" "}
                 Parachute Recovery Systems <i className="bi bi-arrow-down"></i>
               </Link>
             </div>
