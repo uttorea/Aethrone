29e57ee1a7463c9a048ea9dcfe1f20f736cba3a1
Nikhil Rai <nikhilrai662@gmail.com>
2024-08-13 09:05:54 +0530

    3

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure the output directory is correctly set (default is 'dist')
-  },
-  server: {
-    open: true, // This opens the browser automatically when you run the dev server
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // Alias for easier imports
-    },
-  },
-  optimizeDeps: {
-    include: ['react-router-dom'], // Ensures react-router-dom is pre-bundled for faster development
-  },
-  // Adding the fallback configuration to ensure proper routing on Vercel
-  // esbuild: {
-  //   jsxInject: `import React from 'react'`, // Automatically imports React in all JSX files
-  // },
-});
+})
